ec9798ae1a6d6d0add71bf1c02f61effb99675f3
Mohammad Jabir <mohammadj94@gmail.com>
2024-06-07 13:27:25 -0600

    exercise complete

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(3.3m, 5.5m));
+            Console.WriteLine(Add(1, 0, true));
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool flag)
+        {
+            string dollars = a + b == 1 ? " dollar" : " dollars";
+            return flag ? $"{a + b}{dollars}" : $"{a + b}";
         }
     }
 }
